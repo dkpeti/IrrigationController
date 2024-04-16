7201ecb82091a56dcb844278b4434e7190cb8c3a
dkpeti <dkpetya@gmail.com>
2024-04-16 16:55:57 +0200

    PiAdd osztály kommentjei frissítése

--- IrrigationController/IrrigationController/View/PiAdd.xaml.cs
+++ IrrigationController/IrrigationController/View/PiAdd.xaml.cs
@@ -17,19 +17,22 @@ namespace IrrigationController
         {
             InitializeComponent();
         }
+
+        // Menti a felhasználó által megadott Raspberry Pi adatokat a szerverre
         public async void SaveClicked(object sender, EventArgs args)
         {
-            if (String.IsNullOrEmpty(txtPiNev.Text))
+            if (String.IsNullOrEmpty(txtPiNev.Text))                                // Ellenőrzi, hogy a név mező nem üres-e
             {
-                await DisplayAlert("Error", "A név nem lehet üres!", "Ok");
+                await DisplayAlert("Hiba", "A név nem lehet üres!", "Ok");          
                 return;
             }
-            else if (String.IsNullOrEmpty(txtPiAzonosito.Text))
+            else if (String.IsNullOrEmpty(txtPiAzonosito.Text))                     // Ellenőrzi, hogy az azonosító mező nem üres-e
             {
-                await DisplayAlert("Error", "Az azonosító nem lehet üres!", "Ok");
+                await DisplayAlert("Hiba", "Az azonosító nem lehet üres!", "Ok");   
                 return;
             }
 
+            // Létrehoz egy új Pi objektumot a felhasználó által megadott adatokkal
             var vPi = new Pi()
             {
                 Nev = txtPiNev.Text,
@@ -39,19 +42,21 @@ namespace IrrigationController
             try
             {
                 IsBusy = true;
-                var response = await App.PiService.CreatePiItemAsync(vPi);
-                switch (response.Status)
+                var response = await App.PiService.CreatePiItemAsync(vPi);       // Menteni az új Pi-t a szerverre
+                
+                // A válasz alapján megfelelő visszajelzés megjelenítése
+                switch (response.Status)    
                 {
                     case Status.SUCCESS:
                         {
-                            CrossToastPopUp.Current.ShowCustomToast($"{txtPiNev.Text} Pi sikeresen hozzáadva", bgColor: "#636363", txtColor: "white", ToastLength.Short);
-                            await Navigation.PopAsync();
-                            await Navigation.PushAsync(new PiData(response.Data));
+                            CrossToastPopUp.Current.ShowCustomToast($"{txtPiNev.Text} Pi sikeresen hozzáadva", bgColor: "#636363", txtColor: "white", ToastLength.Short);       // Felugró értesítés
+                            await Navigation.PopAsync();                                // A PiAdd oldalról visszalép
+                            await Navigation.PushAsync(new PiData(response.Data));      // Tovább lép az új PiData oldalra amely tartalmazza a mentett adatokat
                             break;
                         }
                     case Status.OTHER_ERROR:
                         {
-                            await DisplayAlert("Error", response.StatusString, "Ok");
+                            await DisplayAlert("Hiba", response.StatusString, "Ok");
                             break;
                         }
                 }
