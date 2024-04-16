53858477768f3fc9a25245861944f1faeae7ac54
dkpeti <dkpetya@gmail.com>
2024-04-16 16:00:42 +0200

    Bejelentkezes osztály kommentjei frissítése

--- IrrigationController/IrrigationController/View/Bejelentkezes.xaml.cs
+++ IrrigationController/IrrigationController/View/Bejelentkezes.xaml.cs
@@ -11,40 +11,47 @@ namespace IrrigationController
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Bejelentkezes : BasePage
     {
-        public ICommand Feltetelek => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+        //Megnyitja a felhasználási feltételeket böngészőben (url a Bejelentkezes.xaml oldalon)
+        public ICommand Feltetelek => new Command<string>(async (url) => await Launcher.OpenAsync(url));    
+        
         public Bejelentkezes()
         {
             InitializeComponent();
             BindingContext = this;
-            IsBusy = false;
-            //a toolbar elrejtése a bejelentkezések oldalon
-            NavigationPage.SetHasNavigationBar(this, false);
+            IsBusy = false;                                     // Az alkalmazás nem foglalt
+            NavigationPage.SetHasNavigationBar(this, false);    // Toolbar elrejtése a bejelentkezési oldalon
         }
 
+        // Bejelentkezés gombra kattintás eseménykezelője
         public void LoginClicked(object sender, EventArgs args)
         {
-            IsBusy = true;
-            App.LoginService.LoginWithGoogle(OnLoginSucceeded, OnLoginFailed, OnLoginCanceled);
+            IsBusy = true;      
+            App.LoginService.LoginWithGoogle(OnLoginSucceeded, OnLoginFailed, OnLoginCanceled);     // Google bejelentkezés szolgáltatásának meghívása
         }
 
+        // Sikeres bejelentkezés eseménykezelője
         private async void OnLoginSucceeded(User user)
         {
-            Navigation.InsertPageBefore(new TabbedMainPage(), this);
+            Navigation.InsertPageBefore(new TabbedMainPage(), this);    // A bejelentkezési oldal után a TabbedMainPage jelenik meg a felhasználónak
             await Navigation.PopAsync();
         }
 
+        // A bejelentkezés megszakítva eseménykezelője (ha a felhasználó kilép a bejelentkezési ablakból anélkül, hogy bejelentkezne)
         private void OnLoginCanceled()
         {
             IsBusy = false;
         }
 
+        // Sikertelen bejelentkezés eseménykezelője
+        // Hibaüzenet megjelenítése
         private async void OnLoginFailed(string reason)
         {
             IsBusy = false;
-            await DisplayAlert("Error", reason, "Ok");
+            await DisplayAlert("Hiba", reason, "Ok");
         }
     }
 
+    // Hitelesítő szolgáltató
     public enum AuthProvider
     {
         Google
