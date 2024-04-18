806659b279be292cdbf2f26e963b586329c80f34
dkpeti <dkpetya@gmail.com>
2024-04-18 15:41:12 +0200

    SzenzorEdit osztály kommentjei frissítése

--- IrrigationController/IrrigationController/View/SzenzorEdit.xaml.cs
+++ IrrigationController/IrrigationController/View/SzenzorEdit.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using IrrigationController.Model;
 using IrrigationController.Service;
+using Plugin.Toast;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,23 +22,26 @@ namespace IrrigationController
             BindingContext = this.Szenzor;
         }
 
+        // Menti a felhasználó által szerkesztett szenzor adatokat a szerverre
         private async void SaveClicked(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(txtSzenzorNev.Text))
             {
-                await DisplayAlert("Hiba", "A név nem lehet üres!", "Ok");
+                await DisplayAlert("Hiba", "A név nem lehet üres!", "Ok");          // Ellenőrzi, hogy a név mező nem üres-e
                 return;
             }
             else if (String.IsNullOrEmpty(txtSzenzorMegjegyzes.Text))
             {
-                await DisplayAlert("Hiba", "A megjegyzés nem lehet üres!", "Ok");
+                await DisplayAlert("Hiba", "A megjegyzés nem lehet üres!", "Ok");   // Ellenőrzi, hogy az azonosító mező nem üres-e
                 return;
             }
             var response = await App.SzenzorService.EditSzenzorItemAsync(Szenzor);
+
+            // A válasz alapján megfelelő visszajelzés megjelenítése
             switch (response.Status)
             {
                 case Status.SUCCESS:
-                    {
+                    {                       
                         await Navigation.PopAsync();
                         break;
                     }
